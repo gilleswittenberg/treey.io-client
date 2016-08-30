29303a5c2aa0a542e0e6d5d0b1b5f54e5b132d08
Gilles Wittenberg <git@gilleswittenberg.com>
2016-08-30 15:58:16 +0800

    Escape to unedit

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -3,14 +3,36 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 import Node from '../components/Node.jsx'
 import AddForm from '../components/AddForm.jsx'
+import { unsetIsEditing } from '../actions/ui'
 
 class App extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
+
+  componentDidMount () {
+    window.addEventListener('keyup', this.handleKeyPress)
+  }
+
+  handleKeyPress (event) {
+    switch (event.key) {
+    case 'Escape':
+      this.handleEscapePress()
+    }
+  }
+
+  handleEscapePress () {
+    const { dispatch } = this.props
+    dispatch(unsetIsEditing())
+  }
 
   render () {
     const { nodes, ui, dispatch } = this.props
     const { tree } = nodes
     const { _id: id = null, title = '', nodes: treeNodes = [] } = tree
-    
+
     return (
       <div className="wrap">
         <ul>
