6f53c78a7d3532a2e8ebf25385f292892dc4ad4e
Gilles Wittenberg <git@gilleswittenberg.com>
2016-10-20 19:13:32 +0800

    Flow ImmutableArray

--- app/lib/ImmutableArray.js
+++ app/lib/ImmutableArray.js
@@ -1,14 +1,16 @@
+/* @flow */
+
 import { List } from 'immutable'
 
 const ImmutableArray = {
 
-  add (arr, value) {
+  add (arr: [], value: any) : [] {
     const list = new List(arr)
     const newList = list.includes(value) ? list : list.push(value)
     return newList.toArray()
   },
 
-  remove (arr, value) {
+  remove (arr: [], value: any) : [] {
     const list = new List(arr)
     const index = list.findIndex(val => val === value)
     const newList = index > -1 ? list.remove(index) : list
